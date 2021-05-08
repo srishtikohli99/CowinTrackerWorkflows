278d119d048435e8d05ef756d11f540f753d127c
Srishti Kohli <46334368+srishtikohli99@users.noreply.github.com>
2021-05-09 02:44:57 +0530

    added headers

--- driver.py
+++ driver.py
@@ -4,7 +4,8 @@ from dateutil.tz import gettz
 
 todays_date = datetime.datetime.now(tz=gettz('Asia/Kolkata')).strftime("%d-%m-%Y")
 URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=110052&date=" + str(todays_date)
-r=requests.get(url=URL)
+headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+r=requests.get(url=URL, headers = headers)
 #print(r)
 data = r.json()
 for centre in data["centers"]:
