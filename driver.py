64778c9626285cccd2dffb3ccc68a3b8c59252c9
Srishti Kohli <46334368+srishtikohli99@users.noreply.github.com>
2021-05-09 02:47:19 +0530

    added ubuntu header

--- driver.py
+++ driver.py
@@ -4,7 +4,7 @@ from dateutil.tz import gettz
 
 todays_date = datetime.datetime.now(tz=gettz('Asia/Kolkata')).strftime("%d-%m-%Y")
 URL = "https://cdn-api.co-vin.in/api/v2/appointment/sessions/public/calendarByPin?pincode=110052&date=" + str(todays_date)
-headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/50.0.2661.102 Safari/537.36'}
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0'}
 r=requests.get(url=URL, headers = headers)
 #print(r)
 data = r.json()
